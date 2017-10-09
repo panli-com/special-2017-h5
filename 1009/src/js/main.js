2571e7ebf1514f01316397ca70fabb91cd5d2e48
zanjs <root@zanjs.com>
2017-10-09 13:43:56 +0800

    Winners do what losers don't want to do :princess:

--- 1009/src/js/main.js
+++ 1009/src/js/main.js
@@ -1,33 +1,4 @@
 //main  
-
-// var notyf = new Notyf();
-function navClickIndex(index) {
-  PD(".floor").addClass("hide");
-  PD("#floor_" + index).removeClass("hide");
-}
-
-function LayerSpe(ele){
-  var ele = ele || "#Lay-main-1";
-
-  var Lay = PD(ele).html();
-
-  PL.open({
-      type: 1,
-      title: false,
-      closeBtn: false,
-      area: '9.333333rem',
-      skin: 'layui-Pan-nobg', //没有背景色
-      shadeClose: true,
-      content: Lay
-  });
-
-
-  PD(".l-close").on("click",function(){
-      PL.closeAll();
-  })
-
-}
-
 // ;
 (function() {
   // Create an instance of Notyf
@@ -37,13 +8,13 @@ function LayerSpe(ele){
 
     FastClick.attach(document.body);
 
-    PD('#nav-tap').on('click','li',function(){
-      var t = PD(this);
-      var index = t.index()
-      PD("#nav-tap li").removeClass('cur')
-      t.addClass('cur')
-      navClickIndex(index + 1)
-    })
+    var oneSrc = document.getElementById("one-src").src + '?v'
+    var time = new Date().getTime()
    
+    var oneBox = document.getElementById("one-box")
+    var src = oneSrc + time 
+    var str = '<img src="'+ src +'" />'
+
+    oneBox.innerHTML = str
   }
 })()
